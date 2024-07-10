09bb1f472554d3eb67d3073427f4cd11d7217764
CodingHerWay <dev@ankitayadav.me>
2024-07-10 17:25:40 +0530

    HTTP methods

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const fs = require("fs");
 const url = require("url");
 
 const myServer = http.createServer((req, res) => {
-  const log =`${Date.now()}: ${req.url} New req recieved\n`;
+  const log =`${Date.now()}:${req.method} ${req.url} New req recieved\n`;
   const myUrl = url.parse(req.url , true);
   console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
     switch(myUrl.pathname) { // myUrl.pathname // req.url
-      case "/": res.end("HomePage");
+      case "/":
+      if (req.method ==="GET") res.end("Welcome to Home Page");
+    
       break;
       case "/about" : 
       const username = myUrl.query.myname
@@ -18,6 +20,13 @@ const myServer = http.createServer((req, res) => {
       case "/search":
         const search = myUrl.query.search_query;
         res.end("Here are your results for" + search);
+
+        case "/signup":
+          if (req.method ==="GET") res.end("This is a signup form");
+          else if (req.method ==="POST") {
+            // DB query
+            res.end("Thanks for signing up");
+          }
       default:
        res.end("404 Page");
     }
