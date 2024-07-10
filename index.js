56eb7f95f12d7d99130cc84c162108b21298d5df
CodingHerWay <dev@ankitayadav.me>
2024-07-11 00:43:29 +0530

    using express

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const express = require("express");
+
+const app = express(); // app handler function he hai
+
+app.get("/", (req, res) => {
+  return res.send("Hello from home page");
+});
+
+app.get("/about", (req, res) => {
+  return res.send("Hello from about page");
+});
+
+function myHandler(req, res) {
 
 const myServer = http.createServer((req, res) => {
   const log =`${Date.now()}:${req.method} ${req.url} New req recieved\n`;
@@ -33,6 +46,10 @@ const myServer = http.createServer((req, res) => {
   });
  
 });
+}
+
+//const myServer = http.createServer(myHandler);
+const myServer = http.createServer(app);
 
 myServer.listen(8000, () =>{
   console.log("server started")
